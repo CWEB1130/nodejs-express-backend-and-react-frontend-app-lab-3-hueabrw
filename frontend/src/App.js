0efc651615fd0cdc02e69053b6e7cc546f247363
Abraham Huerta <hueabrw@dunwoody.edu>
2020-04-17 20:43:04 -0500

    Changed readme

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+/*
+
+
+    Submition by: Abe Huerta
+    Submition date: 4/17/2020
+
+
+*/
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
